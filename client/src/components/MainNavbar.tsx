337dce952ed02f3f3f1e0f89cdacd34c365f619c
Danilo <danilomaioli@gmail.com>
2023-11-20 20:58:21 -0300

    update

--- client/src/components/MainNavbar.tsx
+++ client/src/components/MainNavbar.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 
 function MainNavbar() {
     return (
+        <nav className="navbar navbar-expand-lg" style={{background: "#8ea5ff"}}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">LetDM</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default MainNavbar;
+
+/*
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">LetDM</a>
@@ -11,7 +30,7 @@ function MainNavbar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
@@ -38,7 +57,4 @@ function MainNavbar() {
                 </div>
             </div>
         </nav>
-    );
-}
-
-export default MainNavbar;
+*/
