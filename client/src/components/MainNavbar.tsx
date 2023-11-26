e92eac4f5fb0c7dd9457e26640e4eb50512a4f92
Danilo <danilomaioli@gmail.com>
2023-11-26 14:53:28 -0300

    updated logo

--- client/src/components/MainNavbar.tsx
+++ client/src/components/MainNavbar.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 function MainNavbar() {
     return (
-        <nav className="navbar navbar-expand-lg" style={{background: "#8ea5ff"}}>
+        <nav className="navbar navbar-expand-lg" style={{background: "#e3e5ff"}}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">LetDM</a>
+                {/*<a className="navbar-brand" href="/">LetDM</a>*/}
+                <img style={{width: "50px"}} src="logo.png" alt="Logo" />;
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto">
                         
                     </ul>
                 </div>
